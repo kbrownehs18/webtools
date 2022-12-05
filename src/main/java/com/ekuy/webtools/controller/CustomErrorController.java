69e7ef3d5038160ba7d536006c592fe1a8bac08a
zhuchenglin <scnjl@qq.com>
2022-12-05 14:36:45 +0800

    [fix]

--- src/main/java/com/ekuy/webtools/controller/CustomErrorController.java
+++ src/main/java/com/ekuy/webtools/controller/CustomErrorController.java
@@ -2,22 +2,17 @@ package com.ekuy.webtools.controller;
 
 import com.ekuy.webtools.model.Result;
 import com.ekuy.webtools.model.ResultCodeEnum;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("")
-@Slf4j
+@RequestMapping
 public class CustomErrorController implements ErrorController {
-    @RequestMapping("/error")
+    @RequestMapping({"${server.error.path:${error.path:/error}}"})
     public Result handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        return new Result(ResultCodeEnum.REQUEST_ERROR, status.toString());
+        return new Result(ResultCodeEnum.NOT_FOUND);
     }
 }
